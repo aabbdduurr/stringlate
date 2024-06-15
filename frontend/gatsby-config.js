c66ba1021a3b515d91deb5ec3dd5db402f735800
Abdur Rahman <77055976+aabbdduurr@users.noreply.github.com>
2024-06-15 12:44:17 +0530

    typescript, gatsby, express, nodemon, gitgore updates, etc

---
+++ frontend/gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby Site",
+    description: "A new Gatsby site.",
+    author: "@gatsbyjs",
+  },
+  plugins: ["gatsby-plugin-typescript"],
+};
